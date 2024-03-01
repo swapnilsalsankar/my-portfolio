b57806aec5f5d6ed05dd539cccc366e5d1d54b74
Swapnil salsankar <swapnilsalsankar@Swapnils-MacBook-Air.local>
2024-02-29 22:12:55 -0500

    projects added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,19 +11,24 @@ const Project = () => {
       <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4'>
       <div class="shadow-md shadow-[#040c16] group container rounded-md  flex flex-col justify-between text-center items-center mx-auto py-2 px-4 overflow-auto">
         <div>
-          <h3 class="pt-3 text-2xl font-bold text-[#1e3751] ">Modern Real Estate Marketplace </h3>
+          <h3 class="pt-3 text-2xl font-bold text-[#1e3751] ">DevSecOps-Netflix-Clone </h3>
           <p class="text-[#6a7280]">2023</p>
           <div class="pt-1 flex flex-wrap justify-center gap-0 items-center">
-            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">ReactJS</p>
-            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">NodeJS</p>
-            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">ExpressJS</p>
-            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">MongoDB</p>
-            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">JWT</p>
-            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">REST APIs</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">Kubernetes</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">Prometheus</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">Grafana</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">Jenknins</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">AWS</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">SonarQube</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">Trivy</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">Helm</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">ArgoCD</p>
+
           </div>
-          <p class="py-2 text-[#6a7280]">This project was built with the MERN stack—utilizing React, Node.js, Express.js, MongoDB, 
-          JWT authentication, and Rest APIs, alongside Tailwind CSS. 
-          It's a web application featuring advanced authentication and robust search functionality..</p>
+          <p class="py-2 text-[#6a7280]">Implemented a robust DevSecOps pipeline for a High streaming platform by
+           Leveraging Kubernetes, Prometheus, Grafana, Jenkins, SonarQube, Trivy, Argo CD, and Helm, I optimized resource utilization while adhering to OWASP standards. 
+           Highlights include seamless integration of Helm charts and Argo CD, real-time monitoring with Prometheus and Grafana, and automated deployment and code inspection with Jenkins and SonarQube. 
+          These initiatives ensured security, scalability, and efficiency throughout the software development lifecycle..</p>
         </div>
         <div class="flex text-center ">
           <a href="https://github.com/PatidarAsh/Mern-Estate.git" target="_blank" rel="noreferrer">
@@ -36,15 +41,22 @@ const Project = () => {
       </div>
       <div class="shadow-md shadow-[#040c16] group container rounded-md  flex flex-col justify-between text-center items-center mx-auto py-2 px-4 overflow-auto">
         <div>
-          <h3 class="pt-3 text-2xl font-bold text-[#1e3751] ">Amazon Community</h3>
-          <p class="text-[#6a7280]">2023</p>
+          <h3 class="pt-3 text-2xl font-bold text-[#1e3751] ">CICD-using-CodePipeline</h3>
+          <p class="text-[#6a7280]">2024</p>
           <div class="pt-1 flex flex-wrap justify-center gap-0 items-center">
-            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">Figma</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">CodeCommit</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">CodeBuild</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">CodeDeploy</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">CodePipeline</p>
+            <p class="text-xs text-[#1e3751] border-[#6a7280] border rounded-lg m-1 p-1 hover:bg-[#1e3751] hover:text-white">Kubernetes</p>
+
+
           </div>
-          <p class="py-2 text-[#6a7280]">This UX project involved integrating Twitter-like functionality into the Amazon app, focusing on tasks such as live sessions, group shopping, following users, and product searches with hashtags, with a key emphasis on clear navigation and intuitive design.</p>
+          <p class="py-2 text-[#6a7280]">Led the development and implementation of a robust CI/CD pipeline on AWS, integrating key services including CodeCommit, CodeBuild, CodeDeploy, and CodePipeline.
+           Leveraged AWS S3 for artifact storage, ensuring scalability and reliability, while implementing stringent security measures using AWS KMS for authentication.</p>
         </div>
         <div class="flex text-center ">
-          <a href="https://www.figma.com/proto/gtRe3osceIdUf9ueCqArGE/Ashvini_Patidar?type=design&node-id=685-412&viewport=1748%2C2031%2C0.39&t=b76cJn1lTgRDykzM-0&scaling=min-zoom&starting-point-node-id=689%3A618" target="_blank" rel="noreferrer">
+          <a href="https://github.com/swapnilsalsankar/CICD_using_CodePipeline.git" target="_blank" rel="noreferrer">
             <button class="rounded-sm text-white bg-[#C3073F] px-3 py-1 my-3 mx-1 flex items-center hover:bg-[#1e3751]">Code</button>
           </a>
           <a href="https://www.figma.com/proto/gtRe3osceIdUf9ueCqArGE/Ashvini_Patidar?type=design&node-id=573-1345&viewport=-545%2C-1113%2C0.23&t=XYEr4rCvJC4pbIgb-0&scaling=scale-down&starting-point-node-id=574%3A1351" target="_blank" rel="noreferrer">
